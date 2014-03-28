3093b703faf5f5dc0e291c0acbd476ca308122b4
Jake Sorce <jake@instructure.com>
2014-03-28 09:22:48 -0600

    update rails version and app config

--- config/application.rb
+++ config/application.rb
@@ -19,5 +19,8 @@ module JoeSorceConstruction
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+    config.action_dispatch.default_headers = {
+        'X-Frame-Options' => 'ALLOWALL'
+    }
   end
 end
